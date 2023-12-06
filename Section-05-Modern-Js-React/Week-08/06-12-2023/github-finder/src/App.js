284270f1bb449b10ab4105ecc75cc8d1539083a7
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2023-12-06 21:57:32 +0300

    6 aralik ders sonu

---
+++ Section-05-Modern-Js-React/Week-08/06-12-2023/github-finder/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+
+    </>
+  );
+}
+
+export default App;
---
+++ Section-05-Modern-Js-React/Week-08/06-12-2023/github-finder/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <>
+            <div>
+                <h1>Welcome</h1>
+                <p>Bu uygulama ile Github Profillerini arayabilirsiniz.</p>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
