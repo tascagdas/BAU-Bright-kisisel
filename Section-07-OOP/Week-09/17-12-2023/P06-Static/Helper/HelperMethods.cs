dec59ea668da6d0d20c679cb09b6da7aab5667f1
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2023-12-19 21:53:57 +0300

    19 aralık ders sonu

---
+++ Section-07-OOP/Week-09/17-12-2023/P06-Static/Helper/HelperMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace P06_Static.Helper
+{
+	public static class HelperMethods
+	{
+		public static string EditText(string text)
+			{
+			string result = text.Replace("?", " ");
+			return result;
+			}
+		
+	}
+}
+
--- Section-07-OOP/Week-09/17-12-2023/P06-Static/Program.cs
+++ Section-07-OOP/Week-09/17-12-2023/P06-Static/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace P06_Static;
-
+﻿using P06_Static.Helper;
+namespace P06_Static;
+public static class Deneme
+{
+    public static int Sayi { get; set; }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Random rnd = new Random();
+        string name = "Cagdas?Tas";
+        //HelperMethods helper = new HelperMethods();
+        //Console.WriteLine(helper.EditText(name));
+        Console.WriteLine(HelperMethods.EditText(name));
+
     }
 }
 
