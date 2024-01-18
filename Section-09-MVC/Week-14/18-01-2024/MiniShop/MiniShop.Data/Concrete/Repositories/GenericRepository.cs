8d0f70960155372c4b9214130c91319f9267c106
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-01-18 18:44:46 +0300

    Ders oncesi hocadan dosyayi cektim.

---
+++ Section-09-MVC/Week-14/18-01-2024/MiniShop/MiniShop.Data/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniShop.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data.Concrete.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext _dbContext;
+
+        public GenericRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            await _dbContext
+                .Set<TEntity>()
+                .AddAsync(entity);
+            await _dbContext
+                .SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            List<TEntity> entities = await _dbContext
+                .Set<TEntity>()
+                .ToListAsync();
+            return entities;
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            TEntity entity = await _dbContext
+                .Set<TEntity>()
+                .FindAsync(id);
+            return entity;
+        }
+
+        public void HardDelete(TEntity entity)
+        {
+            _dbContext
+                .Set<TEntity>()
+                .Remove(entity);
+            _dbContext .SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext
+                .Set<TEntity>()
+                .Update(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
