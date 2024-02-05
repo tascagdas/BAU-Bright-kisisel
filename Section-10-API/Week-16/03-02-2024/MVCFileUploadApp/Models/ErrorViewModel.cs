15a1a41002dc0f133c61c55a21fddb2e08a27bf5
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-02-05 13:36:57 +0300

    cumartesi günü projesinden resim upload için olan proje silindi.

--- Section-10-API/Week-16/03-02-2024/MVCFileUploadApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVCFileUploadApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
