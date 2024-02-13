3d452bb0f765c91cf4ee8fe6235933c960a6ab27
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-02-13 19:06:24 +0300

    Rider üzerinden günün proje eklemesi yapıldı.

---
+++ Section-11-Identity/Week-18/13-02-2024/MiniShop/MiniShop.Entity/Abstract/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Entity.Abstract
+{
+    public abstract class BaseEntity
+    {
+        public DateTime ModifiedDate { get; set; }= DateTime.Now;
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; }
+        public string Url { get; set; }
+    }
+}
