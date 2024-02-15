19cab71860ea8993add52fc644637513dacedf50
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-02-15 19:04:27 +0300

    ders oncesi takip ve proje oluisturma

---
+++ Section-11-Identity/Week-18/15-02-2024/MiniShop/MiniShop.Shared/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,13 @@
+namespace MiniShop.Shared.ViewModels;
+
+public class ShoppingCartViewModel
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public List<ShoppingCartItemViewModel> ShoppingCartItems { get; set; }
+
+    public decimal TotalPrice()
+    {
+        return ShoppingCartItems.Sum(x => x.ProductPrice * x.Quantity);
+    }
+}
