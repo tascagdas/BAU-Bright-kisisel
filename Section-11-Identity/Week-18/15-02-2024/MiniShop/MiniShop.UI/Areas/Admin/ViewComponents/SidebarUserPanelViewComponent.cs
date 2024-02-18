61399c4e419ead488a27ce62958f5e69621d442e
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-02-18 14:10:42 +0300

    Admin Panelde sidebar'da user bilgilerini gösterme halledildi.

--- Section-11-Identity/Week-18/15-02-2024/MiniShop/MiniShop.UI/Areas/Admin/ViewComponents/SidebarUserPanelViewComponent.cs
+++ Section-11-Identity/Week-18/15-02-2024/MiniShop/MiniShop.UI/Areas/Admin/ViewComponents/SidebarUserPanelViewComponent.cs
@@ -2,14 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MiniShop.Entity.Concrete.Identity;
 
 namespace MiniShop.UI.Areas.Admin.ViewComponents
 {
     public class SidebarUserPanelViewComponent : ViewComponent
     {
+        private readonly UserManager<User> _userManager;
+
+        public SidebarUserPanelViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var user = _userManager.GetUserAsync(UserClaimsPrincipal);
+
+            ViewBag.firstName = user.Result.FirstName;
+            ViewBag.lastName = user.Result.LastName;
             return View();
         }
     }
