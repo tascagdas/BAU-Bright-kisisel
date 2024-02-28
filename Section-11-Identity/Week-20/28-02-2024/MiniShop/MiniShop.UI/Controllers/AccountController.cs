e511b508aea0b94f6d1ebe4fa7be15aee89c50fc
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-02-28 21:58:42 +0300

    Sifre degistirme ve profil bilgilerini degistirme yapildi.

--- Section-11-Identity/Week-20/28-02-2024/MiniShop/MiniShop.UI/Controllers/AccountController.cs
+++ Section-11-Identity/Week-20/28-02-2024/MiniShop/MiniShop.UI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using MiniShop.Business.Abstract;
 using MiniShop.Entity.Concrete.Identity;
 using MiniShop.Shared.ViewModels;
@@ -105,16 +106,99 @@ public class AccountController : Controller
     {
         var userId = _userManager.GetUserId(User);
         var orders = await _orderManager.GetOrdersAsync(userId);
-        
+        var user = await _userManager.FindByIdAsync(userId);
 
         //simdilik view e bir order listesi yolluyoruz sonraki asamalarda bu liste ile birlikte baska bilgilerde yollanicak.
 
         UserProfileViewModel userProfileViewModel = new UserProfileViewModel
         {
-            User = null,
+            User = new UserViewModel
+            {
+                Id = userId,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Address = user.Address,
+                City = user.City,
+                Gender = user.Gender,
+                DateOfBirth = user.DateOfBirth
+            },
             Orders = orders
         };
         
         return View(userProfileViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Profile(UserProfileViewModel userProfileViewModel)
+    {
+        var userId = _userManager.GetUserId(User);
+        var user = await _userManager.FindByIdAsync(userId);
+        if (ModelState.IsValid)
+        {
+            user.UserName = userProfileViewModel.User.UserName;
+            user.FirstName = userProfileViewModel.User.FirstName;
+            user.LastName = userProfileViewModel.User.LastName;
+            user.Email = userProfileViewModel.User.Email;
+            user.City = userProfileViewModel.User.City;
+            user.PhoneNumber = userProfileViewModel.User.PhoneNumber;
+            user.DateOfBirth = userProfileViewModel.User.DateOfBirth;
+            user.Gender = userProfileViewModel.User.Gender;
+            
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                await _userManager.UpdateSecurityStampAsync(user);
+                await _signInManager.SignOutAsync();
+                await _signInManager.SignInAsync(user, false);
+                return Redirect("~/");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("",error.Description);
+            }
+            
+        }
+        userProfileViewModel.Orders=await _orderManager.GetOrdersAsync(userId);
+        return View(userProfileViewModel);
+    }
+
+    public async Task<IActionResult> ChangePassword()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> ChangePassword(ChangePasswordViewModel changePasswordViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            var user =await _userManager.FindByIdAsync(_userManager.GetUserId(User));
+            var isVerified = await _userManager.CheckPasswordAsync(user, changePasswordViewModel.OldPassword);
+            if (isVerified)
+            {
+                var result = await _userManager.ChangePasswordAsync(user, changePasswordViewModel.OldPassword,
+                    changePasswordViewModel.NewPassword);
+                if (result.Succeeded)
+                {
+                    var updateSecurityStampResult = await _userManager.UpdateSecurityStampAsync(user);
+                    await _signInManager.SignOutAsync();
+                    await _signInManager.PasswordSignInAsync(user, changePasswordViewModel.NewPassword, false, false);
+                    return RedirectToAction("Profile");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+
+                return View(changePasswordViewModel);
+            }
+            ModelState.AddModelError("","gecerli sifreniz hatalidir!");
+            return View(changePasswordViewModel);
+        }
+        return View(changePasswordViewModel);
     }
 }
